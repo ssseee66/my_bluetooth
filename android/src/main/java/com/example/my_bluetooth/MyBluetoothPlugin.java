7caf3ae1ebe12bdc8e16e0afe792f57289394a12
噜噜噜 <feiyuanaichirou@outlook.com>
2024-09-23 00:31:56 +0800

    asa

--- android/src/main/java/com/example/my_bluetooth/MyBluetoothPlugin.java
+++ android/src/main/java/com/example/my_bluetooth/MyBluetoothPlugin.java
@@ -128,7 +128,8 @@ public class MyBluetoothPlugin implements FlutterPlugin,  MethodCallHandler {
                                     }
                                     
                                     boolean notity = device.setNotify(true);
-                                    maps.put("epcApperOverMessage", maps.get("epcAppearOverMessage") + notity);
+                    
+                                    maps.put("epcApperOverMessage", maps.get("epcAppearOverMessage").toString() + notity);
                                     flutter_channel.send(maps);
                                 }
                             });
